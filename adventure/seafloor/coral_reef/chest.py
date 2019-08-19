89012d7836131607afe3603a42a4efa95c63d881
jennylihan <jlhan@stanford.edu>
2019-08-19 19:12:13 +0800

    Update chest.py

--- adventure/seafloor/coral_reef/chest.py
+++ adventure/seafloor/coral_reef/chest.py
@@ -10,7 +10,7 @@ def treasure():
 
 def monster1():
     print("    *rumble* *rumble* *rumble* \n")
-    print("    You look around the reef, but nothing seems out of place.\n")
+    print("    You look around the reef and you notice treasure has appeared!\n")
 
 def monster2():
     print("    *rumble* *rumble* *rumble* \n")
@@ -18,8 +18,8 @@ def monster2():
     print("    Without looking you grab the treasure from the chest.")
     print("    Quick, use mkdir to make a \"bag\" directory and mv to")
     print("    hide the treasure in the bag. ")
-    print("    Then get back to the surface ASAP! The moster is coming! \n")
-    print("    Don't forget to take your treasure bag with you!")
+    print("    Then get back to the surface ASAP! The monster is coming! \n")
+    print("    Don't forget to take your treasure bag with you up to the top directory!")
     now = datetime.datetime.now()
     with open(".timer", "w") as keyfile:
         keyfile.write(str(now))
